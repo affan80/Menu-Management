52792e99ad59eba1644f7e74f60d944c9c9199a3
affan80 <mrzaidzaid202@gmail.com>
2025-03-14 23:46:42 +0530

    first commit

---
+++ HotelMenu.py
@@ -0,0 +1,39 @@
+menu = {
+    "pizza": 250,
+    "pasta": 200,
+    "burger": 150,
+    "fries": 200,
+    "coffee": 250
+} 
+
+print("Welecome to PIY restaurant")
+print("pizza: Rs.250\n pasta: Rs. 200\n burger: Rs.150\n fries: Rs.200\n coffee: Rs.250")
+
+item_1 = input("Enter the name of item you want to order: ")
+
+total_oreder = 0
+
+if item_1 in menu:
+    total_oreder += menu [item_1]
+   
+
+else:
+    print("item not found in menu")
+
+print(f"your order of {item_1} is added to your order")
+
+
+another_item = input("Do you want to order another item? (Y/N): ")
+
+for i in range(1,len(menu)):
+    if another_item == "Y":
+            item_2 = input("Enter the name of item you want to order: ")
+            if item_2 in menu:
+                total_oreder += menu[item_2]
+                print(f"item {item_2} has been added to your order")
+            else:
+                print("item not found in menu") 
+
+
+
+print("The total cost of your order is: ", total_oreder)
